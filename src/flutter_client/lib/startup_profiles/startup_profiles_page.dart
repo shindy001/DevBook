f1fe7847dcb5cf16d4f0cbd08dd29785bd26a4ed
shindy001 <sindelar.p.etr@seznam.cz>
2023-10-04 23:59:05 +0200

    feat(FlutterClient): add datatable of startup profiles to startup profiles page

--- src/flutter_client/lib/startup_profiles/startup_profiles_page.dart
+++ src/flutter_client/lib/startup_profiles/startup_profiles_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_client/repositories/startup_profiles/startup_profiles_repository.dart';
 import 'package:flutter_client/ui/widgets/page_with_navigation.dart';
+
+import '../_app/app_provider.dart';
+import '../ui/theme/theme.dart';
 
 class StartupProfilesPage extends StatelessWidget {
   const StartupProfilesPage({super.key});
@@ -12,9 +16,60 @@ class StartupProfilesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const PageWithNavigation(
-      activeSideMenuItemName: 'Startup Profiles',
-      child: Text('Hello Startup Profiles'),
+    final startupProfilesRepository = AppProvider.of<StartupProfilesRepository>(context);
+
+    return ListenableBuilder(
+      listenable: startupProfilesRepository,
+      builder: (context, child) => PageWithNavigation(
+        activeSideMenuItemName: 'Startup Profiles',
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(DevBookSpacing.sm),
+              child: Text(
+                'Startup Profiles',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Card(
+                    clipBehavior: Clip.antiAlias,
+                    child: SingleChildScrollView(
+                      child: DataTable(
+                        columnSpacing: 20,
+                        columns: const [
+                          DataColumn(label: Text('Id')),
+                          DataColumn(label: Text('Name')),
+                          DataColumn(label: Text('AppSetupIds')),
+                          DataColumn(label: Text('Actions')),
+                        ],
+                        rows: [
+                          for (var item in startupProfilesRepository.startupProfiles)
+                            DataRow(cells: [
+                              DataCell(Text(item.id)),
+                              DataCell(Text(item.name)),
+                              DataCell(ConstrainedBox(
+                                constraints: const BoxConstraints(maxWidth: 200),
+                                child: Text(
+                                  item.appSetupsIds?.join(', ') ?? '',
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              )),
+                              const DataCell(Text('')),
+                            ])
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
