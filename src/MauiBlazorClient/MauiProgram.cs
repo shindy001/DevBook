5c453597c64a50d730d0b946994767785ee0eb5b
shindy001 <sindelar.p.etr@seznam.cz>
2023-10-22 15:12:43 +0200

    feat(MauiBlazorClient): initial project setup with basic menu and mudblazor lib

---
+++ src/MauiBlazorClient/MauiProgram.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Microsoft.Maui.Platform;
+using MudBlazor.Services;
+
+namespace MauiBlazorClient
+{
+	public static class MauiProgram
+	{
+
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+
+			builder.Services.AddMauiBlazorWebView();
+			builder.Services.AddMudServices();
+
+#if DEBUG
+			builder.Services.AddBlazorWebViewDeveloperTools();
+			builder.Logging.AddDebug();
+#endif
+
+#if WINDOWS
+			builder.ConfigureLifecycleEvents(events =>
+			{
+				events.AddWindows(windowLifecycleBuilder =>
+				{
+					windowLifecycleBuilder.OnWindowCreated(window =>
+					{
+						var minWindowWidth = 960;
+						var minWindowHeigh = 660;
+						var restrictedWindow = new Window()
+						{
+							Width = minWindowWidth,
+							Height = minWindowHeigh,
+							MinimumWidth = minWindowWidth,
+							MinimumHeight = minWindowHeigh,
+							MaximumWidth = double.PositiveInfinity,
+							MaximumHeight = double.PositiveInfinity
+						};
+						window.UpdateSize(restrictedWindow);
+						window.UpdateMinimumSize(restrictedWindow);
+						window.UpdateMaximumSize(restrictedWindow);
+					});
+				});
+			});
+#endif
+
+			return builder.Build();
+		}
+	}
+}
