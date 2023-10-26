d667bb09f781326c4f2f6f05ef5acce32a2522b8
shindy001 <sindelar.p.etr@seznam.cz>
2023-10-26 22:38:36 +0200

    refactor(MauiBlazorClient): move component logic to separate files

---
+++ src/MauiBlazorClient/Features/AppSetups/CreateOrUpdateDialog.razor.cs
@@ -0,0 +1,84 @@
+﻿using MauiBlazorClient.Services;
+using MediatR;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace MauiBlazorClient.Features.AppSetups;
+
+public partial class CreateOrUpdateDialog
+{
+	[CascadingParameter] private MudDialogInstance _mudDialog { get; set; } = default!;
+	[Inject] private IMediator Mediator { get; set; } = default!;
+	[Inject] private IFilePickerService FilePickerService { get; set; } = default!;
+
+	[Parameter] public AppSetups.Model.AppSetup? AppSetup { get; set; }
+
+	private string _id = string.Empty;
+	private string _name = string.Empty;
+	private string _path = string.Empty;
+	private string? _arguments = string.Empty;
+
+	private MudForm _form = default!;
+	private bool _isValidForm;
+
+	protected override void OnParametersSet()
+	{
+		_id = AppSetup?.Id ?? string.Empty;
+		_name = AppSetup?.Name ?? string.Empty;
+		_path = AppSetup?.Path ?? string.Empty;
+		_arguments = AppSetup?.Arguments ?? string.Empty;
+	}
+
+	private async Task SubmitOnEnterPress(KeyboardEventArgs e)
+	{
+		if (e.Code == "Enter")
+		{
+			await Submit();
+		}
+	}
+
+	private async Task Submit()
+	{
+		await _form.Validate();
+		if (_isValidForm)
+		{
+			await Mediator.Send(new Command { Id = _id, Name = _name, Path = _path, Arguments = _arguments });
+			_mudDialog.Close(DialogResult.Ok(true));
+		}
+	}
+
+	private void Cancel() => _mudDialog.Cancel();
+
+	private async Task PickPath()
+	{
+		var (success, path) = await FilePickerService.PickOneAsync();
+		if (success)
+		{
+			_path = path;
+		}
+	}
+
+	public record Command : IRequest
+	{
+		public string? Id { get; set; }
+		public required string Name { get; set; }
+		public required string Path { get; set; }
+		public string? Arguments { get; set; }
+	}
+
+	public class Handler(IAppSetupsService _appSetupsService) : IRequestHandler<Command>
+	{
+		public async Task Handle(Command request, CancellationToken cancellationToken)
+		{
+			if (string.IsNullOrWhiteSpace(request.Id))
+			{
+				await _appSetupsService.Create(request.Name, request.Path, request.Arguments);
+			}
+			else
+			{
+				await _appSetupsService.Update(request.Id, request.Name, request.Path, request.Arguments);
+			}
+		}
+	}
+}
