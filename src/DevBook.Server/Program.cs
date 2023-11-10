e42a116bfe4c76fbe3d96b86bc0af0fc7732599b
shindy001 <sindelar.p.etr@seznam.cz>
2023-11-10 23:47:57 +0100

    chore: add DevBook.sln to project, move proto files to src, add grpc reflection to server, migrate server to .net 8

--- src/DevBook.Server/Program.cs
+++ src/DevBook.Server/Program.cs
@@ -2,14 +2,16 @@ using DevBook.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.MapGrpcReflectionService();
+}
+
 app.MapGrpcService<AppSetupsService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
