1dc8252ebbee9dcb6828ab634f2101cf4bde8b83
shindy001 <sindelar.p.etr@seznam.cz>
2023-11-17 21:34:11 +0100

    feat(DevBook.Server): add GetAppSetups handler with test response

---
+++ src/DevBook.Server/Features/Profiles/AppSetupsService.cs
@@ -0,0 +1,34 @@
+using DevBook.Grpc;
+using DevBook.Shared.Contracts;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace DevBook.Server.Features.Profiles;
+
+internal sealed class AppSetupsService(IExecutor _executor) : AppSetupsGrpcService.AppSetupsGrpcServiceBase
+{
+	public override async Task<GetAllResponse> GetAll(GetAllRequest request, ServerCallContext context)
+	{
+		return await _executor.ExecuteQuery(new GetAppSetups());
+	}
+
+	public override Task<GetByIdResponse> GetById(GetByIdRequest request, ServerCallContext context)
+	{
+		return base.GetById(request, context);
+	}
+
+	public override Task<Empty> Create(CreateRequest request, ServerCallContext context)
+	{
+		return base.Create(request, context);
+	}
+
+	public override Task<Empty> Update(UpdateRequest request, ServerCallContext context)
+	{
+		return base.Update(request, context);
+	}
+
+	public override Task<Empty> Delete(DeleteRequest request, ServerCallContext context)
+	{
+		return base.Delete(request, context);
+	}
+}
---
+++ src/DevBook.Server/Features/Profiles/GetAppSetups.cs
@@ -0,0 +1,16 @@
+﻿using DevBook.Grpc;
+using DevBook.Shared.Contracts;
+
+namespace DevBook.Server.Features.Profiles;
+
+internal class GetAppSetups : IQuery<GetAllResponse>;
+
+internal class GetAppSetupHandler : IQueryHandler<GetAppSetups, GetAllResponse>
+{
+	public async Task<GetAllResponse> Handle(GetAppSetups request, CancellationToken cancellationToken)
+	{
+		var response = new GetAllResponse();
+		response.Items.AddRange([new AppSetupDto { Id = Guid.NewGuid().ToString(), Name = "Karel", Path = "Path", Arguments = "Args" }]);
+		return await Task.FromResult(response);
+	}
+}
